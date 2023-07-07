86f6e90534386f78d851bab715f0f1a6649af7ec
eorfanakis <106256967+manos996@users.noreply.github.com>
2023-07-07 18:49:32 +0300

    Update diffusion_coefficient_script.py

--- diffusion_coefficient_script.py
+++ diffusion_coefficient_script.py
@@ -16,5 +16,29 @@ plt.show()
 
 # Fit the curve to calculate diffusion coefficient 
 coeff = np.polyfit(time, msd, 1)
-diffusion_coeff = coeff[0] / 6.0
+slope = coeff[0]
+intercept = coeff[1]
+
+# Calculate R-squared value
+residuals = msd - (slope * time + intercept)
+ssr = np.sum(residuals**2)
+sst = np.sum((msd - np.mean(msd))**2)
+r_squared = 1 - (ssr / sst)
+
+# Convert time units to ns for left and right tau limits
+left_tau_limit = min(time) / 1000  # Conversion from ps to ns
+right_tau_limit = max(time) / 1000  # Conversion from ps to ns
+
+# Calculate standard error
+std_err = np.sqrt(ssr / (len(time) - 2))
+
+# Calculate diffusion coefficient
+diffusion_coeff = slope / 6.0
+
+print('Left Tau Limit: {:.3f} ns'.format(left_tau_limit))
+print('Right Tau Limit: {:.3f} ns'.format(right_tau_limit))
+print('Standard Error: {:.6f} m^2/s'.format(std_err))
+print('Slope: {:.6f} A^2/ns'.format(slope))
+print('Y-intercept: {:.6f} A^2'.format(intercept))
+print('R-squared: {:.6f}'.format(r_squared))
 print('Diffusion Coefficient: {:.6f} nm^2/ps'.format(diffusion_coeff))
